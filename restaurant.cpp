ec2922150c6bfaffc4fdb734ffb0f4381583578d
calrose <48226669+calrose@users.noreply.github.com>
2021-04-03 16:27:31 -0400

    Update restaurant.cpp

--- restaurant.cpp
+++ restaurant.cpp
@@ -1,1 +1,19 @@
+#include "restaurant.h"
 
+
+Test::Test()
+{
+	name = "";
+}
+
+
+string Test::getName()
+{
+	return name;
+}
+
+string Test::setName(string rName)
+{
+	name = rName;
+	return name;
+}
