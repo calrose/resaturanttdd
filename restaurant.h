f0f630a3354d9d260f6857179263fd6a067f4517
calrose <48226669+calrose@users.noreply.github.com>
2021-04-03 16:28:14 -0400

    Update restaurant.h

--- restaurant.h
+++ restaurant.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+class Test
+{
+public:
+
+	Test();
+	//~MyClass();
+	string getName();
+	string setName(string name);
+private:
+	string name;
+};
