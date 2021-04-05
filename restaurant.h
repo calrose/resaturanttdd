86a62c7e8f5c26975c3fd883af563e1bd336cc85
calrose <48226669+calrose@users.noreply.github.com>
2021-04-05 11:57:33 -0400

    Restaurant.cpp was causing issues with unit testing

--- restaurant.h
+++ restaurant.h
@@ -15,3 +15,20 @@ public:
 private:
 	string name;
 };
+
+Test::Test()
+{
+	name = "";
+}
+
+
+string Test::getName()
+{
+	return name;
+}
+
+string Test::setName(string rName)
+{
+	name = rName;
+	return name;
+}
